21bd5ba8356cf24b23ed7efe20c43751f216993f
Peter Bösenberg <peterboesenberg@gmail.com>
2019-11-28 14:04:01 +0400

    Testing knn with all columns

--- titanic.py
+++ titanic.py
@@ -3,17 +3,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-
+import operator
 
 
 def prepare_general_data():
     df = pd.read_csv('train.csv')
     df = df.dropna()
     df['Sex'] = df['Sex'].astype("category").cat.codes
+    df['Embarked'] = df['Embarked'].astype("category").cat.codes
     y = df['Survived']
     return df, y
 
-def get_knn_score(X_train, X_test, y_train, y_test,neighbors = 10):
+def get_knn_score(X_train, X_test, y_train, y_test,neighbors = 9):
     model = KNeighborsClassifier(n_neighbors = neighbors)
     model.fit(X_train, y_train)
     knn_score = model.score(X_test, y_test)
@@ -23,31 +24,50 @@ def set_features(features):
     x = df[features]
     return train_test_split(x, y, random_state=0)
 
-def get_single_column_knn_score(column):
+def get_one_knn_scores(column):
     X_train, X_test, y_train, y_test = set_features(column)
     score = get_knn_score(X_train, X_test, y_train, y_test)
     knn_scores.update({', '.join(column): score})
 
 
-def get_single_column_knn_scores():
-    get_single_column_knn_score(['Age'])
-    get_single_column_knn_score(['Pclass'])
-    get_single_column_knn_score(['SibSp'])
-    get_single_column_knn_score(['Parch'])
-    get_single_column_knn_score(['Fare'])
-    get_single_column_knn_score(['Sex'])
-    get_single_column_knn_score(['Age', 'Fare'])
-    get_single_column_knn_score(['SibSp', 'Parch'])
-    get_single_column_knn_score(['Age', 'Parch'])
-    get_single_column_knn_score(['Fare', 'Parch'])
-    get_single_column_knn_score(['Sex', 'Fare'])
-    get_single_column_knn_score(['Sex','Pclass'])
+def get_all_knn_scores():
+    get_one_knn_scores(['Age'])
+    get_one_knn_scores(['Pclass'])
+    get_one_knn_scores(['SibSp'])
+    get_one_knn_scores(['Parch'])
+    get_one_knn_scores(['Fare'])
+    get_one_knn_scores(['Sex'])
+    get_one_knn_scores(['Embarked'])
+    get_one_knn_scores(['Age', 'Pclass'])
+    get_one_knn_scores(['Age', 'SibSp'])
+    get_one_knn_scores(['Age', 'Parch'])
+    get_one_knn_scores(['Age', 'Fare'])
+    get_one_knn_scores(['Age', 'Sex'])
+    get_one_knn_scores(['Age', 'Embarked'])
+    get_one_knn_scores(['Pclass', 'SibSp'])
+    get_one_knn_scores(['Pclass', 'Parch'])
+    get_one_knn_scores(['Pclass', 'Fare'])
+    get_one_knn_scores(['Pclass', 'Sex'])
+    get_one_knn_scores(['Pclass', 'Embarked'])
+    get_one_knn_scores(['Parch', 'SibSp'])
+    get_one_knn_scores(['Parch', 'Fare'])
+    get_one_knn_scores(['Parch', 'Sex'])
+    get_one_knn_scores(['Parch', 'Embarked'])
+    get_one_knn_scores(['SibSp', 'Fare'])
+    get_one_knn_scores(['SibSp', 'Sex'])
+    get_one_knn_scores(['SibSp', 'Embarked'])
+    get_one_knn_scores(['Fare', 'Sex'])
+    get_one_knn_scores(['Fare', 'Embarked'])
+
+    get_one_knn_scores(['Sex', 'Embarked'])
 
 
 df, y = prepare_general_data()
 # X_train, X_test, y_train, y_test = set_features()
 
 knn_scores = {}
-get_single_column_knn_scores()
-print(knn_scores)
+get_all_knn_scores()
+
+maximum = max(knn_scores.items(), key=operator.itemgetter(1))
+print(maximum[0],maximum[1])
 
